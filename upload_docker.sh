398cbd35af229fc0a0caf2be319a45fee0221038
Richard Irving <richard.irving3@dwp.gov.uk>
2023-03-11 14:58:02 +0000

    upload_docker.sh amended successfully - Task 4 Complete

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="riccardopixel/udacity-build-app:v1"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
