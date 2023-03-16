7f9f322c2ef269e147e9289e35a851a82a35e307
Richard Irving <richard.irving3@dwp.gov.uk>
2023-03-16 08:10:03 +0000

    Movement of terraform files

--- .circleci/files/main.tf
+++ .circleci/files/main.tf
@@ -1,25 +1,102 @@
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-
-  name   = "udacity"
-  region = "us-east-1"
-  tags = {
-    Name      = local.name
-    Terraform = "true"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.22.0"
+    }
   }
 }
 
-module "vpc" {
-  source     = "./modules/vpc"
-  cidr_block = "10.100.0.0/16"
+provider "aws" {
+  region     = "us-east-1"
+}
 
-  account_owner = local.name
-  name          = "${local.name}-project"
-  azs           = ["us-east-1a", "us-east-1b"]
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+resource "aws_security_group" "configure_access" {
+  name        = "configure-access"
+  description = "Allow SSH inbound traffic"
+  #vpc_id      = module.vpc.vpc_id
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+
+      description     = ""
+      from_port       = 22
+      prefix_list_ids = []
+      protocol        = "tcp"
+      security_groups = []
+      self            = false
+      to_port         = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+
+      description     = ""
+      from_port       = 80
+      prefix_list_ids = []
+      protocol        = "tcp"
+      security_groups = []
+      self            = false
+      to_port         = 80
+    },{
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+
+      description     = ""
+      from_port       = 8000
+      prefix_list_ids = []
+      protocol        = "tcp"
+      security_groups = []
+      self            = false
+      to_port         = 8000
+    },
+  ]
+
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+
+      description     = ""
+      from_port       = 80
+      to_port         = 80
+      prefix_list_ids = []
+      protocol        = "tcp"
+      security_groups = []
+      self            = false
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+
+      description     = ""
+      from_port       = 443
+      to_port         = 443
+      prefix_list_ids = []
+      protocol        = "tcp"
+      security_groups = []
+      self            = false
+    },
+  ]
+
+  tags = {
+    Name = "ssh_access"
   }
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "curl-instance"
+
+  #ami                    = data.aws_ami.canonical_ubuntu.id
+  ami                    = "ami-0557a15b87f6559cf"
+  key_name               = "ric-key-pair"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.configure_access.id]
+  #subnet_id              = module.vpc.public_subnet_ids[0]
+
 }
