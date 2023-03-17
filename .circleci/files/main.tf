6af17f60fcd902ad754aca0ce2925de4acab816e
Richard Irving <richard.irving3@dwp.gov.uk>
2023-03-17 11:40:55 +0000

    aws s3 ls test

--- .circleci/files/main.tf
+++ .circleci/files/main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "configure_access" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
-  name = "curl-instance"
+  name = "deploy-instance"
 
   #ami                    = data.aws_ami.canonical_ubuntu.id
   ami                    = "ami-0557a15b87f6559cf"
@@ -99,6 +99,6 @@ module "ec2_instance" {
   vpc_security_group_ids = [aws_security_group.configure_access.id]
   #subnet_id              = module.vpc.public_subnet_ids[0]
   tags ={
-    "Name" = "curl-instance-${var.ID}"
+    "Name" = "deploy-instance-${var.ID}"
   }
 }
